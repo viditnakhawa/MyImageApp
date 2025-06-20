8c32dc1c0d44b92ed14b068c21e8e838e1c44b7d
Vidit <vidit.nakhawa24@gmail.com>
2025-06-20 22:27:32 +0530

    Fixed Logic

--- app/src/main/java/com/viditnakhawa/myimageapp/ui/common/ViewModelProvider.kt
+++ app/src/main/java/com/viditnakhawa/myimageapp/ui/common/ViewModelProvider.kt
@@ -1,7 +1,7 @@
 package com.viditnakhawa.myimageapp.ui.common
 
+import androidx.work.WorkManager
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.viditnakhawa.myimageapp.MyApplication
@@ -10,29 +10,22 @@ import com.viditnakhawa.myimageapp.ui.modelmanager.ModelManagerViewModel
 
 object ViewModelProvider {
     val Factory = viewModelFactory {
-        // Initializer for ModelManagerViewModel (This part is correct)
         initializer {
-            val application = this.myApplication()
+            val application = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MyApplication)
             ModelManagerViewModel(
                 downloadRepository = application.container.downloadRepository,
                 dataStoreRepository = application.container.dataStoreRepository,
-                context = application.container.context,
+                context = application.applicationContext
             )
         }
 
         initializer {
-            val application = this.myApplication()
+            val application = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MyApplication)
             ImageViewModel(
-                // The constructor only needs the repository, which we get from the container.
-                repository = application.container.imageRepository
+                repository = application.container.imageRepository,
+                workManager = WorkManager.getInstance(application.applicationContext),
+                applicationContext = application.applicationContext
             )
         }
     }
-}
-
-/**
- * Extension function to get the MyApplication instance from CreationExtras.
- */
-fun CreationExtras.myApplication(): MyApplication {
-    return (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MyApplication)
 }
